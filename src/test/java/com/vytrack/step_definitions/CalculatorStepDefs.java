2b29592d07956b72c1c84883b2e3d00462a1f813
Ali-1977000 <ali_hasib2003@yahoo.com>
2021-10-01 12:28:20 +0100

    create new feature file

--- src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/CalculatorStepDefs.java
@@ -8,17 +8,19 @@ public class CalculatorStepDefs {
 
     @Given("I have calculator app open")
     public void i_have_calculator_app_open() {
-
+        System.out.println("Running step : I have calculator app open");
 
     }
 
     @When("I add {int} to {int}")
-    public void i_add_to(Integer int1, Integer int2) {
+    public void i_add_to(Integer num1, Integer num2) {
+        System.out.println("Running step : I add " + num1 + " to " + num2);
 
     }
 
     @Then("I should get {int}")
-    public void i_should_get(Integer int1) {
+    public void i_should_get(Integer expected) {
+        System.out.println("Running step : Verifying the sum should be " + expected);
 
     }
 
