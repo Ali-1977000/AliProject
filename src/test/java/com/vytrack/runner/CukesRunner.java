97c5f32df3c53ed37ceec6824d6a656b7b7ce63d
Ali-1977000 <ali_hasib2003@yahoo.com>
2021-10-01 12:29:32 +0100

    update CukesRunner

--- src/test/java/com/vytrack/runner/CukesRunner.java
+++ src/test/java/com/vytrack/runner/CukesRunner.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-              features = "src/test/resources/features",
+              features = "src/test/resources/features",  // tell location of the feature file
 
-               glue = "com/vytrack/step_definitions"
+               glue = "com/vytrack/step_definitions",   //tell where the code is
+               dryRun = false  // check if we have missing steps or not
 
 )
 
