b37bb99a92f6016e74703b29a13ac20634b809e1
Ali-1977000 <ali_hasib2003@yahoo.com>
2021-10-01 12:30:07 +0100

    files updated

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDef.java
@@ -0,0 +1,23 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class LoginStepDef {
+
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+
+
+    }
+
+    @When("the user enters the driver information")
+    public void theUserEntersTheDriverInformation() {
+
+    }
+
+    @Then("the user should be able to login")
+    public void theUserShouldBeAbleToLogin() {
+
+    }
+
+}
