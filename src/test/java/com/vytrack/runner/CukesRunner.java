0a81d4c5e1b67eecaeb0aa264de28c50cd5038be
Ali-1977000 <ali_hasib2003@yahoo.com>
2021-10-04 01:14:12 +0100

    added "rerun:target/rerun.txt" to remember failed features scenario

--- src/test/java/com/vytrack/runner/CukesRunner.java
+++ src/test/java/com/vytrack/runner/CukesRunner.java
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"
         } ,
-        tags = "@wip3"  //@calculator"   // "@login"  // which feature you want to run
+        tags = "@wip4"  //@calculator"   // "@login"  // which feature you want to run
 )
 public class CukesRunner {
 
